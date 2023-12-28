34296e52c63061ca2377af2020cb70da98247f0a
sergeyshkolnik <144228619+sergeyshkolnik@users.noreply.github.com>
2023-12-28 16:30:59 +0600

    wkdh

--- returnbooks.php
+++ returnbooks.php
@@ -1,3 +1,12 @@
 <?php
 include_once("connection.php");
-
+session_start();
+$_SESSION['user'] = $user;
+$stmt = $conn->preapre("SELECT tblusers.Forename as fn , tblbooks.BookName as bn FROM TblUserBooks
+INNER JOIN TblUsers 
+ON TblUsers.UserID=TblUserBooks.UserID
+INNER JOIN TblBooks 
+ON TblBooks.BookID=TblUserBooks.BookID
+DELETE FROM TblUserBooks WHERE fn = %$user%")
+$stmt->execute();
+echo("You returned your books");
