0466fee2c8ab9c115b41d9845250488f9333da2f
sergeyshkolnik <shkolnik.s@oundleschool.org.uk>
2024-01-01 23:12:31 +0300

    good library

--- welcome.php
+++ welcome.php
@@ -2,11 +2,14 @@
 <html>
     <head>
         Oundle School Library
-        <link rel = "stylesheet" href = "
+        <link rel = "stylesheet" href = "library.css">
     </head>
     <body>
+        <div class = "welcome">
         <h1>Welcome to the OS Library</h1>
+        <img src="OUNDLE_LOGO-300x294.jpg" alt="LOGO">
         <p>This is a library database for students and members of staff</p>
-        <a href="login.php">Log In</a>
+        <a href="login.php" class = "btn btn-dark" role = "button"> Login</a>
+        </div>
     </body>
 </html>
