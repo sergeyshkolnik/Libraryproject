9eff26b498561503704b21897925a6ebcb81a862
Sergey Shkolnik <shkolnik.s@oundleschool.org.uk>
2024-01-01 17:21:21 +0000

    librar

--- returnbooks.php
+++ returnbooks.php
@@ -1,2 +1,16 @@
+<!DOCTYPE html>
 <?php
 include_once("connection.php");
+session_start();
+
+$stmt1 = $conn->prepare("DELETE TblUserBooks, TblUsers
+FROM TblUserBooks
+JOIN TblUsers ON TblUserBooks.UserID = TblUsers.UserID
+WHERE Surname = :user");
+$stmt1->bindParam(':user', $_SESSION['user']);
+$stmt1->execute();
+echo("You returned your books")
+?>
+<html>
+    <a href="useroptions.php">Back to Menu</a>
+</html>
--- useroptions.php
+++ useroptions.php
@@ -3,20 +3,23 @@
     <body>
         <h1>User Page</h1>
         <a href="mybooks.php">View My Books</a>
-        <a href="reuturnbooks.php">Return Books</a>
+        <a href="returnbooks.php">Return Books</a>
+        <h2>Your Books:</h2><br>
     </body>
 </html>
 <?php
 include_once('connection.php');
 session_start();
 
-$stmt1 = $conn->prepare("SELECT tblusers.Surname as sn WHERE Surname = :user , tblbooks.BookName as bn FROM TblUserBooks
+$stmt1 = $conn->prepare("SELECT tblusers.Forename as sn , tblbooks.BookName as bn FROM TblUserBooks
 INNER JOIN TblUsers 
 ON TblUsers.UserID=TblUserBooks.UserID
 INNER JOIN TblBooks 
-ON TblBooks.BookID=TblUserBooks.BookID");
+ON TblBooks.BookID=TblUserBooks.BookID
+WHERE Surname = :user");
 $stmt1->bindParam(':user', $_SESSION['user']);
 $stmt1->execute();
+
 while($row = $stmt1->fetch(PDO::FETCH_ASSOC) ) {
     
     
