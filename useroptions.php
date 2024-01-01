cfc37785ce89dd9b1d2c763b24948f6067c18959
Sergey Shkolnik <shkolnik.s@oundleschool.org.uk>
2024-01-01 09:43:46 +0000

    lighli

--- useroptions.php
+++ useroptions.php
@@ -6,3 +6,20 @@
         <a href="reuturnbooks.php">Return Books</a>
     </body>
 </html>
+<?php
+include_once('connection.php');
+session_start();
+
+$stmt1 = $conn->prepare("SELECT tblusers.Surname as sn WHERE Surname = :user , tblbooks.BookName as bn FROM TblUserBooks
+INNER JOIN TblUsers 
+ON TblUsers.UserID=TblUserBooks.UserID
+INNER JOIN TblBooks 
+ON TblBooks.BookID=TblUserBooks.BookID");
+$stmt1->bindParam(':user', $_SESSION['user']);
+$stmt1->execute();
+while($row = $stmt1->fetch(PDO::FETCH_ASSOC) ) {
+    
+    
+    echo($row["bn"]."<br>");
+
+}
