fec3229250a32daf49c98590c8fd287f3f2ea0ea
sergeyshkolnik <shkolnik.s@oundleschool.org.uk>
2024-01-04 16:37:13 +0300

    css

--- adminoptions.php
+++ adminoptions.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
+<title>Oundle School Library</title>
+        <link rel = "stylesheet" href = "library.css">
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 <html>
     <body>
         <h1>Admin Page</h1>
         <a href="selectuserbooks.php"> Loan out book</a>
         <a href="viewbooks.php">View Loaned books</a>
-        <a href="users.php">Add a User</a>
+        <a href="users.php" class= "btn">Add a User</a>
         <a href="books.php">Add a Book</a>
         <p>WELCOME</p>
     </body>
