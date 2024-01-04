252e75a2a616c76c78fa4468b5a625f10cb8d395
sergeyshkolnik <shkolnik.s@oundleschool.org.uk>
2024-01-04 16:46:47 +0300

    ilyg

--- useroptions.php
+++ useroptions.php
@@ -1,8 +1,18 @@
 <!DOCTYPE html>
 <html>
+<link rel = "stylesheet" href = "library.css">
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
     <body>
+        <div class = "login jumbotron"></div>
         <h1>User Page</h1>
-        <a href="mybooks.php">View My Books</a>
+        </div>
+        
         <a href="returnbooks.php">Return Books</a>
         <h2>Your Books:</h2><br>
     </body>
